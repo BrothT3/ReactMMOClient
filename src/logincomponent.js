8f144f73585e9c090f73e0797a77068023872a39
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2023-04-11 11:17:50 +0200

    LoginUpdate

--- src/logincomponent.js
+++ src/logincomponent.js
@@ -1,30 +1,63 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import useGameServer from "./useGameServer";
+import { JsonHubProtocol } from "@microsoft/signalr";
+
 
 
 function LogInForm(){
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    
 
 
-const handleChange = (event) => {
+ const handleChange = (event) => {
     setUserName(event.target.userName);
-    setPassword(event.target.password);
+    setPassword(event.target.password);} 
 
-} 
+    async function Login(event) {
+        event.preventDefault();
+        let response = await fetch('http://jats.web.dania-studerende.dk/authentication/login', {
+          method: 'POST',
+          body: JSON.stringify({
+            "username": userName,
+            "password": password
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+    
+         
+        console.log(response);
+        return response;
 
+      }
 
+   
 
-return (
+ return (
 
-    <form>
+    <form onSubmit={Login}>
         <label>
             Name:
-            <input type = "text" value={userName} onChange={handleChange}></input>
+            <input 
+            type = "text" 
+            name="username"
+            value={userName}
+             onChange={handleChange} 
+             autoComplete="off"></input>
         </label>
         <label>
             Password:
-            <input type = "password" value={password} onChange={handleChange}></input>
+            <input 
+            type = "password" 
+            name="password"
+            value={password} 
+            onChange={handleChange}
+            autoComplete="off"></input>
         </label>
+
+        <button type="submit">Login</button>
     </form>
 
 );
