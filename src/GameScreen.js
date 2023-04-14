193cfc69e09ff382f311f3bd0b69adfbbdd3af26
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2023-04-14 11:01:40 +0200

    Update GameScreen.js

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { useState } from "react";
 import useGameServer from "./useGameServer";
-import { getUserAgentHeader } from "@microsoft/signalr/dist/esm/Utils";
+
 
 
 
