ec8a5bff4e9ec37d9a4ff3ccadf25ee8386b8551
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2023-04-18 12:24:42 +0200

    LoginComponentPopup

--- src/logincomponent.js
+++ src/logincomponent.js
@@ -1,66 +1,63 @@
 import React, { useState } from "react";
 
 function LoginScreen(props) {
+  const [loginStatus, setLoginStatus] = useState('Please Enter Your Credentials');
 
   const handleSetData = (newData) => {
     props.setAuthorization(newData);
-  };
 
+    if(!newData.success)
+    {
+      setLoginStatus("Wrong Username Or Password");
+    }
+  };
 
   return (
     <div>
-      <LogInForm setData={handleSetData} />
+      <LogInForm setData={handleSetData} loginStatus={loginStatus} setLoginStatus={setLoginStatus} />
     </div>
   );
-
-
-
+}
 
 function LogInForm(props) {
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const [loginStatus, setLoginStatus] = useState('Please Enter Your Credentials');
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        const response = await fetch('http://jats.web.dania-studerende.dk/authentication/login', {
-          method: 'POST',
-          body: JSON.stringify({
-            username: userName,
-            password: password
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-        const data = await response.json();
-        let userData = {success: data.success, data: data.data, username: userName }
-        props.setData(userData);
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
 
-        if(!data.success)
-        {
-          setLoginStatus("Wrong Username Or Password");
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://jats.web.dania-studerende.dk/authentication/login', {
+        method: 'POST',
+        body: JSON.stringify({
+          username: userName,
+          password: password
+        }),
+        headers: {
+          'Content-Type': 'application/json'
         }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-  
-    const handleUserNameChange = (event) => {
-      setUserName(event.target.value);
-    };
-  
-    const handlePasswordChange = (event) => {
-      setPassword(event.target.value);
-    };
+      });
+      const data = await response.json();
+      let userData = {success: data.success, data: data.data, username: userName }
+      props.setData(userData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
     return (
       <form onSubmit={handleSubmit}>
         <label>
           Name:
-          <input 
-            type="text" 
+          <input
+            type="text"
             name="userName"
             value={userName}
             onChange={handleUserNameChange}
@@ -69,19 +66,28 @@ function LogInForm(props) {
         </label>
         <label>
           Password:
-          <input 
-            type="password" 
+          <input
+            type="password"
             name="password"
-            value={password} 
+            value={password}
             onChange={handlePasswordChange}
             autoComplete="off"
           />
         </label>
-      <h1>{loginStatus}</h1>
+        <h1>{props.loginStatus}</h1>
         <button type="submit">Login</button>
+        {props.loginStatus === "Wrong Username Or Password" && (
+          <div style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.5)", display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <div style={{ backgroundColor: "white", padding: "20px", borderRadius: "5px", textAlign: "center" }}>
+              <h2>Login Failed</h2>
+              <p>Please check your username and password and try again.</p>
+              <button onClick={() => props.setLoginStatus("Please Enter Your Credentials")}>OK</button>
+            </div>
+          </div>
+        )}
       </form>
     );
   }
-}
+
   export default LoginScreen;
   
